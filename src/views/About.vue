a4a0818e5369300db63d4ac510aa21a14119e8f0
ManuelRocha98 <36920329+ManuelRocha98@users.noreply.github.com>
2020-01-27 16:17:32 +0000

    navbar dynamic

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,21 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+export default {
+  name: "about",
+  props: {
+    msg: String
+  },
+  methods: {
+    
+  },
+  created: function(){
+    //Get path
+    this.$store.state.currentPath = window.location.pathname
+    alert(this.$store.state.currentPath)
+    
+  }
+};
+</script>
